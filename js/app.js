b44206c935283d81953f5f925e231e591f4e66ee
User Name <user@email.com>
2022-06-02 19:22:21 -0400

    6b

--- js/app.js
+++ js/app.js
@@ -3,7 +3,8 @@
 let board
 let turn 
 let winner  
- 
+
+
  
 
 /*---------------------------- Variables (state) ----------------------------*/
@@ -13,19 +14,27 @@ let winner
 /*------------------------ Cached Element References ------------------------*/
 
 const squareEls = document.querySelectorAll('.squares')
-const messageEl = document.querySelector('message')
+const messageEl = document.querySelectorAll('.message')
+const gameboard = document.querySelector('.board')
 
-// console.log(squareEls)
+console.log(gameboard)
+console.log(squareEls)
 // console.log(messageEl)
 
 /*----------------------------- Event Listeners -----------------------------*/
 
-init()
+gameboard.addEventListener('click', function(evt){
+evt.handleClick()
+
+})
+console.log(gameboard)
 
 /*-------------------------------- Functions --------------------------------*/
+init()
+
 function init() {
 
-    board = [null, 1, null, -1, null, null, null, null, null,]
+    board = [null, null, null, 1, null, null, null, null, null,]
     turn = 1
     winner = null
 
@@ -53,10 +62,30 @@ function render(){
     }
   } 
   console.log(squareEls)
-  console.log(winner)
-  console.log(turn)
+//   console.log(winner)
+//   console.log(turn)
   
-    
+const winningCombos = 
+    [
+        
+    [0, 1, 2]
+    [0, 3, 6]
+    [3, 4, 5]
+    [6, 7, 8]
+    [1, 4, 7]
+    [2, 5, 8]
+    [0, 4, 8]
+    [2, 4, 6]
+
+]
+
+
+
+
+
+function handleClick(evt) {
+const sqIdx = gameboard[idx]
+ }
 
 
 
